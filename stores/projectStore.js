db0695aa4bc2960d626fdbdd00322c16516ad3ab
Rodrigo Rodrigues <rodrigo.rsmail@gmail.com>
2026-02-24 19:32:29 -0300

    feat(store): add new state properties to projectStore

--- stores/projectStore.js
+++ stores/projectStore.js
@@ -2,25 +2,29 @@ import { defineStore } from 'pinia';
 
 export const useProjectStore = defineStore('project', {
   state: () => ({
-    image: null,
-    name: null,
-    vercelURL: null,
-    githubURL: null,
-    logoURL: null,
+    imageUrl: null,
+    title: null,
+    imagePageUrl: null,
+    technologies: null,
+    vercelUrl: null,
+    githubUrl: null,
+    logoUrl: null,
     year: null,
     category: null,
     description: null
   }),
   actions: {
-    setProjectProps(prop) {
-      this.image = prop.imageURL;
-      this.name = prop.name;
-      this.vercelURL = prop.vercelUrl;
-      this.githubURL = prop.githubUrl;
-      this.logoURL = prop.logoUrl;
-      this.year = prop.year;
-      this.category = prop.category;
-      this.description = prop.description;
+    setProjectProps(projectData) {
+      this.imageUrl = projectData.imageURL;
+      this.title = projectData.title;
+      this.imagePageUrl = projectData.imagePageUrl;
+      this.technologies = projectData.technologies;
+      this.vercelUrl = projectData.vercelUrl;
+      this.githubUrl = projectData.githubUrl;
+      this.logoUrl = projectData.logoUrl;
+      this.year = projectData.year;
+      this.category = projectData.category;
+      this.description = projectData.description;
     }
   },
   persist: true
